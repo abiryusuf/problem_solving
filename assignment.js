9f595bc6515e09ee9baf7a285fe482bfb08caf44
abiryusuf <yusuf_ctg@yahoo.com>
2020-09-07 14:40:37 -0500

    problem solving

--- assignment.js
+++ assignment.js
@@ -7,8 +7,9 @@ function inchToFeet(feet){
     return mile;
 }
 let result = inchToFeet(5280);
-console.log(result);
+console.log(`One mile ${result} feet` );
 
+//wood calculator
 function woodCalculator(chair, table, bed){``
     let chairWood = chair * 1;
     let tableWood = table * 3;
@@ -22,4 +23,55 @@ function woodCalculator(chair, table, bed){``
     return totalWood;
 }
 let result1 = woodCalculator(3,5,6)
-console.log(result1);
+console.log("total wood", result1);
+
+//brick calculator
+function brickCalculator(floor){
+  let brick = 0;
+  let concrete = 1000; // per feet = concrete concrete
+  if(floor <= 0){
+      return "Value can not be negative";
+  }
+  else if(floor <= 10){
+      // 15 feet high from 1 floor to 10th floor 
+      brick = floor * 15 * concrete;
+  }
+  else if (floor <= 20){
+      let firstStep = 10 * 15 * concrete;
+      floor = floor - 10;
+      //12 feet high from 11 to 20th floor
+      brick = firstStep + floor * 12 * concrete;
+  }
+  else {
+    let firstStep = 10 * 15 * concrete;
+    let secondStep = 10 * 12 * concrete;
+    floor = floor - 20;
+    //10 feet high from 20th floor to up
+    brick = firstStep + secondStep + floor * 10 * concrete;
+  }
+  return brick;
+}
+const totalBrick = brickCalculator(21);
+console.log("Total brick", totalBrick);
+
+//tiny friends
+function tinyFriend(friends){
+    if(friends.length === 0){
+        return "Array can not be null";
+    }
+    else {
+        let tiny = friends[0];
+        for (let i = 0; i < friends.length; i++) {
+            const element = friends[i];
+            if(element.length < tiny.length){
+                tiny = element;
+            }
+            
+        }
+        return `Your tiny friend is ${tiny}`;
+    }
+    
+}
+let friendList = ["Abir", "Yusuf", "Mim"]
+let friend = tinyFriend(friendList);
+console.log("Tiny Friend", friend);

27dace0dcb9cb27a65085163bebd0a02b5dd53db
abiryusuf <yusuf_ctg@yahoo.com>
2020-09-07 12:24:50 -0500

    wood calculator

--- assignment.js
+++ assignment.js
@@ -8,3 +8,18 @@ function inchToFeet(feet){
 }
 let result = inchToFeet(5280);
 console.log(result);
+
+function woodCalculator(chair, table, bed){``
+    let chairWood = chair * 1;
+    let tableWood = table * 3;
+    let bedWood = bed * 5;
+
+    if(chair <= 0 || table <= 0 || bed <= 0){
+        return "Quantity can not be negative";
+    }
+
+    let totalWood = chairWood + tableWood + bedWood;
+    return totalWood;
+}
+let result1 = woodCalculator(3,5,6)
+console.log(result1);
